f7f33e5dd874dac65d8d85384f6ef58d7870538f
Reddy Anna and the Art of Following Cricket Match Moments. <reddyanna3007@gmail.com>
2026-02-11 10:43:52 +0530

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,9 +1,1 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0Q02L6FS6W"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-0Q02L6FS6W');
-</script>
